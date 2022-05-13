01454edcc543468e4fcc6863aff542a5438762c7
Joey Bervin <61996287+Joeybervin@users.noreply.github.com>
2022-05-13 09:46:02 +0200

    Mise en place des premières screens
    Mise en place des premières screens de l'app, en vue de commencer le back-end.
    ConnexionScreen
    ConnexionFormScreen
    LikesScreen
    Annonces screen
    ProjectScreen

--- artilyst-backend/routes/index.js
+++ artilyst-backend/routes/index.js
@@ -1,21 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+// My models
+var userModel = require('../models/user');
+var projectModel = require('../models/project');
+
 var request = require('sync-request');
 
 
-// * Create a account
-router.post('/sign-up', function(req, res, next) {
+// * Création d'un compte
+router.post('/sign-up', async function (req, res, next) {
 
+  let alreadyMember = false;
+  /* Véfificaton si le compte éxiste déjà */
+  var user_account = await userModel.findOne({ email: req.body.signUpEmail });
 
-  res.render('index', { title: 'Express' });
+  /* Ajout de l'utilisateur à la base de données */
+  if (!user_account && req.body.signUpEmail !== "" && req.body.signUpPassword !== "" && req.body.signUpUsername !== "") {
+    alreadyMember = true
+    var newUser = new userModel({
+      email: req.body.signUpEmail,
+      password: req.body.signUpPassword,
+    })
+
+    /* J'enregistre dans la base de données */
+    await newUser.save()
+  }
+  
+  res.json({alreadyMember})
+
 });
 
 // * Connexion
-router.post('/sign-in', function(req, res, next) {
+router.post('/sign-in', async function(req, res, next) {
 
+  let alreadyMember = false;
+
+  /* Vérifications des informations données */
+  var account = await userModel.findOne({
+    email: req.body.signInEmail,
+    password: req.body.signInPassword
+  });
+
+
+  if (account !== null) {
+    alreadyMember = true
+    
+  }
+  else {
+    alreadyMember = false
+  }
+ 
+
+  res.json({alreadyMember})
   
-  res.render('index', { title: 'Express' });
 });
 
 
