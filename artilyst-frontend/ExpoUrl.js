7bbe609c4ab2625070062d0da83eefc54e8e2c1c
mustapha chaqri <mchaqri@gmail.com>
2022-05-17 10:11:06 +0200

    commit le merge

--- artilyst-frontend/ExpoUrl.js
+++ artilyst-frontend/ExpoUrl.js
@@ -6,6 +6,6 @@ const expoUrlRaf = "192.168.1.100:3000";
 
 const expoUrlBertin = "192.168.1.100:3000";
 
-const expoUrlMustafa = "192.168.10.144:3000";
+const expoUrlMustafa = "192.168.10.151:3000";
 
 export  {expoUrlRaf, expoUrlBertin, expoUrlJoey, expoUrlMustafa}
