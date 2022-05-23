457bd1ce1d36987451e8367a89f581c92aef7077
mustapha chaqri <mchaqri@gmail.com>
2022-05-23 10:33:10 +0200

    Merge remote-tracking branch 'origin' into backup2

--- artilyst-frontend/ExpoUrl.js
+++ artilyst-frontend/ExpoUrl.js
@@ -6,6 +6,6 @@ const expoUrlRaf = "192.168.10.134:3000";
 
 const expoUrlBertin = "192.168.10.144:3000";
 
-const expoUrlMustafa = "192.168.1.73:3000";
+const expoUrlMustafa = "192.168.10.129:3000";
 
 export  {expoUrlJoey, expoUrlBertin, expoUrlRaf, expoUrlMustafa}
